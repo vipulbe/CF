fdae914074543a7d96a9a77d4a33137f23fe580a
Vipul <behalpade.vipul@gmail.com>
2025-02-13 21:52:24 +0000

    added leetcode link

--- leetcode/Problem283.java
+++ leetcode/Problem283.java
@@ -1,5 +1,6 @@
 package leetcode;
 //  Move zeros to the end
+// https://leetcode.com/problems/move-zeroes/description/
 // Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 // Note that you must do this in-place without making a copy of the array.
 
